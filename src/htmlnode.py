575dcd02dae1e4bedfbc888a9309dda717fe33f5
Phornsawan Allen <80432959+phornsawan-allen@users.noreply.github.com>
2025-06-02 14:40:13 +0100

    Modified src/htmlnode.py

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -18,3 +18,13 @@ class HTMLNode():
     
     def __repr__(self):
         return f"tag= {self.tag}, value= {self.value}, children= {self.children}, props= {self.props} "
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+           super().__init__(tag, value, children=None, props=props)
+    def to_html(self):
+         if self.value == None:
+             raise ValueError
+         if self.tag == None:
+             return self.value
+         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
