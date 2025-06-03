656db14d4e91b78fd4a90896aae3c1a667e82c64
Phornsawan Allen <80432959+phornsawan-allen@users.noreply.github.com>
2025-06-03 13:47:58 +0100

    Modified src/test_htmlnode.py

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 
 class TestHtmlNode(unittest.TestCase):
@@ -28,6 +28,24 @@ class TestHtmlNode(unittest.TestCase):
         node = LeafNode("b", "Hello, world!")
         self.assertEqual(node.to_html(), "<b>Hello, world!</b>")
 
+    def test_to_html_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span><b>grandchild</b></span></div>",)
+
+    def test_parentnode_with_empty_children(self):
+        parent_node = ParentNode("div", [])
+        self.assertEqual(parent_node.to_html(), "<div></div>")
+
+    def test_no_children(self):
+        with self.assertRaises(ValueError):
+            ParentNode("div", None).to_html()
 
 if __name__ == "__main__":
     unittest.main()
