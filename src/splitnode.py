f6425e928016b5b1334f719e89c9d1619d916086
Phornsawan Allen <80432959+phornsawan-allen@users.noreply.github.com>
2025-06-05 16:55:12 +0100

    Modified src/splitnode.py

--- src/splitnode.py
+++ src/splitnode.py
@@ -8,15 +8,17 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         else:
             split_nodes = node.text.split(delimiter)
             if len(split_nodes) % 2 == 0 :
-                raise Exception ("delimiter is not found")
+                raise ValueError ("delimiter is not found")
             for index, value in enumerate(split_nodes):
                 if index % 2 == 0 and value != "":
                     node_type = TextType.TEXT
+                    value = value.strip()
                     text_nodes = TextNode(value, node_type)
                     new_nodes.append(text_nodes)
                 elif index % 2 != 0 and value != "":
                     node_type = text_type
+                    value = value.strip()
                     text_nodes2 = TextNode(value, node_type)
                     new_nodes.append(text_nodes2)
-                
+    return new_nodes
                     
