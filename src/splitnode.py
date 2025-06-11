e7c2248d78c451af624388e0f41e9a24717f5798
Phornsawan Allen <80432959+phornsawan-allen@users.noreply.github.com>
2025-06-11 14:46:32 +0100

    Modified src/splitnode.py file

--- src/splitnode.py
+++ src/splitnode.py
@@ -1,4 +1,5 @@
 from textnode import TextType, TextNode
+from extract_markdown import extract_markdown_images, extract_markdown_links
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
@@ -22,3 +23,62 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                     new_nodes.append(text_nodes2)
     return new_nodes
                     
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if extract_markdown_images(node.text) == []:
+            new_nodes.append(node)
+        else:
+            extract_node = extract_markdown_images(node.text)
+            first_image = extract_node[0]
+            text = first_image[0]
+            url = first_image[1]
+            nodes = node.text.split(f"![{text}]({url})", 1)  
+            text_node = TextNode(text=nodes[0], text_type=TextType.TEXT)
+            if nodes[0] != "":
+                new_nodes.append(text_node)
+            text_node2 = TextNode(text=text, text_type=TextType.IMAGE, url=url)
+            new_nodes.append(text_node2)
+            if nodes[1] != "":
+                more_nodes = split_nodes_image([TextNode(nodes[1], TextType.TEXT)])
+                new_nodes.extend(more_nodes)
+    return new_nodes
+
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if extract_markdown_links(node.text) == []:
+            new_nodes.append(node)
+        else:
+            extract_node = extract_markdown_links(node.text)
+            first_link = extract_node[0]
+            text = first_link[0]
+            url = first_link[1]
+            nodes = node.text.split(f"[{text}]({url})", 1)
+            text_node = TextNode(text=nodes[0], text_type=TextType.TEXT)
+            if nodes[0] != "":
+                new_nodes.append(text_node)
+            text_node2 = TextNode(text=text, text_type=TextType.LINK, url=url)
+            new_nodes.append(text_node2)
+            if nodes[1] != "":
+                more_nodes = split_nodes_link([TextNode(nodes[1], TextType.TEXT)])
+                new_nodes.extend(more_nodes)
+    return new_nodes
+
+           
+                
+                
+                
+                
+                
+                
+                
+                
+        
+
+
+                
+                
+          
