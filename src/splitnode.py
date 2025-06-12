60d59224a716ef2402ae15016c026642bda5742a
Phornsawan Allen <80432959+phornsawan-allen@users.noreply.github.com>
2025-06-12 14:35:48 +0100

    Modified src/splitnode file

--- src/splitnode.py
+++ src/splitnode.py
@@ -13,12 +13,12 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             for index, value in enumerate(split_nodes):
                 if index % 2 == 0 and value != "":
                     node_type = TextType.TEXT
-                    value = value.strip()
+                    
                     text_nodes = TextNode(value, node_type)
                     new_nodes.append(text_nodes)
                 elif index % 2 != 0 and value != "":
                     node_type = text_type
-                    value = value.strip()
+                    
                     text_nodes2 = TextNode(value, node_type)
                     new_nodes.append(text_nodes2)
     return new_nodes
@@ -68,10 +68,23 @@ def split_nodes_link(old_nodes):
     return new_nodes
 
            
-                
-                
-                
-                
+def text_to_textnodes(text):
+    new_nodes= []
+    if text != "":
+        text_node = TextNode(text=text, text_type=TextType.TEXT)
+        new_nodes.append(text_node)
+        split_bold_delimiter = split_nodes_delimiter(old_nodes=new_nodes, delimiter="**", text_type=TextType.BOLD)
+        new_nodes = split_bold_delimiter 
+        split_italic_delimiter = split_nodes_delimiter(old_nodes=new_nodes, delimiter="_", text_type=TextType.ITALIC)
+        new_nodes = split_italic_delimiter
+        split_code_delimiter = split_nodes_delimiter(old_nodes=new_nodes, delimiter="`", text_type=TextType.CODE) 
+        new_nodes = split_code_delimiter
+
+        split_image = split_nodes_image(new_nodes)
+        new_nodes = split_image     
+        split_link = split_nodes_link(new_nodes)
+        new_nodes = split_link
+    return new_nodes       
                 
                 
                 
